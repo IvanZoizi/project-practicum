ee578fc99e39a29b2a9e3fc174d535c8090251dd
Diana <diana.mashkina.00@gmail.com>
2026-03-03 14:55:21 +0300

    Update Main.java

--- src/main/java/ru/tbank/practicum/Main.java
+++ src/main/java/ru/tbank/practicum/Main.java
@@ -3,6 +3,6 @@ package ru.tbank.practicum;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Goodbye World!"); System.out.println("Goodbye World!"); System.out.println("Goodbye World!");
+        System.out.println("Goodbye World!");
     }
 }
